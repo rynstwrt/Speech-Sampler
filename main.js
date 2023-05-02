08136bc193dc2acec83ee9d16d8ed8f3732332c2
Ryan Stewart <ryanstewartalex@gmail.com>
2023-05-02 16:42:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const textInput = document.querySelector("#text-input");
 const submitButton = document.querySelector("#submit-button");
 
+let playing;
+
 submitButton.addEventListener("click", () =>
 {
+    if (playing) return;
+    playing = true;
+
     const utterance = new SpeechSynthesisUtterance(textInput.value);
+    const voices = window.speechSynthesis.getVoices();
+    utterance.voice = voices[3];
     speechSynthesis.speak(utterance);
+
+    speechSynthesis.addEventListener("end", () =>
+    {
+        playing = false;
+    });
 });
