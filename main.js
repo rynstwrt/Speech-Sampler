bfb4f9b055c99932439528142921d49b6f5c413c
Ryan Stewart <ryanstewartalex@gmail.com>
2023-05-07 21:16:16 -0500

    Add Rate and Pitch Sliders

--- main.js
+++ main.js
@@ -1,20 +1,75 @@
-const textInput = document.querySelector("#text-input");
+const textArea = document.querySelector("#text-area");
+const voiceSelect = document.querySelector("#voice-select");
+const randVoiceButton = document.querySelector("#rand-voice-button");
+const rateInput = document.querySelector("#rate-input");
+const pitchInput = document.querySelector("#pitch-input");
 const submitButton = document.querySelector("#submit-button");
 
-let playing;
+let voices;
+const voiceIndexes = {};
 
-submitButton.addEventListener("click", () =>
+function populateVoiceList()
 {
-    if (playing) return;
-    playing = true;
+    if (typeof speechSynthesis === "undefined") return;
 
-    const utterance = new SpeechSynthesisUtterance(textInput.value);
-    const voices = window.speechSynthesis.getVoices();
-    utterance.voice = voices[3];
-    speechSynthesis.speak(utterance);
+    voices = window.speechSynthesis.getVoices();
 
-    speechSynthesis.addEventListener("end", () =>
+    for (let i = 0; i < voices.length; ++i)
     {
-        playing = false;
-    });
-});
+        const option = document.createElement("option");
+
+        if (voices[i].lang !== "en-US") continue;
+
+        voiceIndexes[voices[i].name.toUpperCase()] = i;
+        option.textContent = voices[i].name.toUpperCase();
+
+        option.setAttribute("data-lang", voices[i].lang);
+        option.setAttribute("data-name", voices[i].name);
+
+        voiceSelect.appendChild(option);
+    }
+}
+
+function speak()
+{
+    const text = (textArea.value) ? textArea.value : "Hello World";
+    const lines = text.split("\n");
+
+    for (let i = 0; i < lines.length; ++i)
+    {
+        const utterance = new SpeechSynthesisUtterance(lines[i]);
+
+        const voiceIndex = voiceIndexes[voiceSelect.value];
+        utterance.voice = voices[voiceIndex];
+        utterance.rate = rateInput.value;
+        utterance.pitch = pitchInput.value;
+
+        speechSynthesis.speak(utterance);
+    }
+}
+
+function selectRandomVoice()
+{
+    const voiceKeys = Object.keys(voiceIndexes);
+    const randomVoice = voiceKeys[Math.floor(Math.random() * voiceKeys.length)];
+
+    const options = Array.from(voiceSelect.options);
+    options.find(o => o.text === randomVoice).selected = true;
+}
+
+function init()
+{
+    populateVoiceList();
+
+    if (typeof speechSynthesis !== "undefined"
+        && speechSynthesis.onvoiceschanged !== undefined)
+    {
+        speechSynthesis.onvoiceschanged = populateVoiceList;
+    }
+}
+
+submitButton.addEventListener("click", () => speak());
+randVoiceButton.addEventListener("click", () => selectRandomVoice());
+
+init();
+
